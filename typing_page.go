302703624ee35bd0bce13372c420412108ffc7f4
hofman <hofman1298@gmail.com>
2023-03-13 19:35:48 +0800

    Reduce quote min length

--- typing_page.go
+++ typing_page.go
@@ -30,12 +30,12 @@ type typingPage struct {
 
 func (t *typingPage) Init() tea.Cmd {
 	//text := "test"
-	text := "hello there how are you my friend?"
+	//text := "hello there how are you my friend?"
 	//text := "During the first part of your life, you only become aware of happiness once you have lost it. Then an age comes, a second one, in which you already know, at the moment when you begin to experience true happiness, that you are, at the end of the day, going to lose it. When I met Belle, I understood that I had just entered this second age. I also understood that I hadn't reached the third age, in which anticipation of the loss of happiness prevents you from living."
 	//text := "‘Margareta! I’m surprised at you! We both know there’s no such thing as love!’"
 	//text := "hey»\nthere"
 
-	//text := getRandomQuote()
+	text := getRandomQuote()
 	words := strings.Split(text, " ")
 
 	t.text = text
